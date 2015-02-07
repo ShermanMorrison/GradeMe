3b31b284da705f2e7f202ce9d818244c5f4f0459
Xilin Liu <xal1@rice.edu>
2015-02-07 15:47:59 -0600

    working file uploads

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 __author__ = 'phrayezzen'
 
 from flask import Flask, request, jsonify
+from werkzeug import secure_filename
 import sqlite3 as lite
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = "/Users/phrayezzen/Documents/Projects/GradeMe/imgtest"
 # con = lite.connect("audio.db", check_same_thread=False)
 # def make_dicts(cursor, row):
 #     return dict((cursor.description[idx][0], value)
@@ -19,5 +21,12 @@ def index():
 def upload():
     return app.send_static_file("upload.html")
 
+@app.route("/uplod", methods=["POST"])
+def uplod():
+	files = request.files.getlist("files[]")
+	for i in files:
+		i.save(app.config['UPLOAD_FOLDER'] + "/" + secure_filename(i.filename))
+	return ('', 204)
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", debug=True)
