ad829063b80af9df9fb31cf67d834eab55043588
Jonathan Wilson <jdw7@rice.edu>
2015-02-07 15:53:42 -0600

    Successfully using jsPDF

--- j.js
+++ j.js
@@ -1,87 +1,37 @@
 "use strict";
 
 
-canvas.width = 600;
-canvas.height = 800;
+function demo() {
+	var doc = new jsPDF();
 
-var ctx = document.getElementById('canvas').getContext('2d');
+	doc.line(20, 20, 60, 20); // horizontal line
+		
+	doc.setLineWidth(0.5);
+	doc.line(20, 25, 60, 25);
 
-// var qPerPage = 10;
-// var totalQ = 23;
+	doc.setLineWidth(1);
+	doc.line(20, 30, 60, 30);
 
-// if ((totalQ < 1)){
-// 	throw "Must have at least one question.";
-// }
-// if (qPerPage > 10){
-// 	qPerPage = 10;
-// }
+	doc.setLineWidth(1.5);
+	doc.line(20, 35, 60, 35);
 
-// var numPages = (totalQ - 1) / qPerPage
-// var lines = qPerPage - 1;
-// var heightQ = canvas.height / lines;
+	doc.setDrawColor(255,0,0); // draw red lines
 
-// //dividing lines
-// for (var i = 0; i < lines; i++){
-// 	ctx.beginPath();
-// 	ctx.moveTo(0, (i+1) * heightQ);
-// 	ctx.lineTo(canvas.width, (i+1) * heightQ);
-// 	ctx.stroke();
-// }
-// //border
-// var offset = [0,0];
-// var w = 40;
-// var h = 20;
+	doc.setLineWidth(0.1);
+	doc.line(100, 20, 100, 60); // vertical line
 
-// ctx.beginPath();
-// ctx.moveTo(offset[0], offset[1]);
-// ctx.lineTo(offset[0] + w, offset[1]);
-// ctx.stroke();
-// ctx.beginPath();
-// ctx.moveTo(offset[0], offset[1] + h);
-// ctx.lineTo(offset[0] + w, offset[1] + h);
-// ctx.stroke();
-// ctx.beginPath();
-// ctx.moveTo(offset[0], offset[1]);
-// ctx.lineTo(offset[0], offset[1] + h);
-// ctx.stroke();
-// ctx.beginPath();
-// ctx.moveTo(offset[0] + w, offset[1]);
-// ctx.lineTo(offset[0] + w, offset[1] + h);
-// ctx.stroke();
+	doc.setLineWidth(0.5);
+	doc.line(105, 20, 105, 60);
 
-var doc = new jsPDF();
+	doc.setLineWidth(1);
+	doc.line(110, 20, 110, 60);
 
-doc.line(20, 20, 60, 20); // horizontal line
 	
-doc.setLineWidth(0.5);
-doc.line(20, 25, 60, 25);
+	// Save the PDF
+	doc.save('Test.pdf');
+}
 
-doc.setLineWidth(1);
-doc.line(20, 30, 60, 30);
-
-doc.setLineWidth(1.5);
-doc.line(20, 35, 60, 35);
-
-doc.setDrawColor(255,0,0); // draw red lines
-
-doc.setLineWidth(0.1);
-doc.line(100, 20, 100, 60); // vertical line
-
-doc.setLineWidth(0.5);
-doc.line(105, 20, 105, 60);
-
-doc.setLineWidth(1);
-doc.line(110, 20, 110, 60);
 
 /////////////////////
 //class definitions//
 /////////////////////
-// function Page(qPerPage, numPages, index){
-// 	this.qPerPage = qPerPage;
-// 	this.numPages = numPages;
-// 	this.index = index;
-// 	this.space = 5; //space b/w pages
-// 	this.width = (canvas.width - ((this.numPages + 1) * this.space))/this.numPages; //?
-
-// 	this.height = 0;  //?
-// }
