2a5c32957b318495283f98e2bf8f92788e028819
Jonathan Wilson <jdw7@rice.edu>
2015-02-07 23:58:55 -0600

    my draw.js is working

--- static/js/draw.js
+++ static/js/draw.js
@@ -1,47 +1,62 @@
 "use strict";
 
 
-var width = 220;
-var height = 290;
 
-var qPerPage = 3;	//input qPerPAge
-var inputArray = ["1 1 1111 11","2222 2222","3333 3 3 3 3 3333","4","5"]; // input string array
-
-var totalQ = inputArray.length;	
-
-if ((totalQ < 1)){
-	throw "Must have at least one question.";
-}
-if (qPerPage > 10){
-	qPerPage = 10;
-}
-
-var numPages = Math.floor((totalQ - 1) / qPerPage) + 1;
-var lines = qPerPage - 1;
-var heightQ = height / qPerPage;
 
 
 ////////////////////
 //global functions//
 ////////////////////
-function drawLines(doc){
+function drawLines(doc,lines,heightQ,width){
 	for (var i = 0; i < lines; i++){
 		doc.setLineWidth(1);
-		doc.line(0, (i+1) * heightQ, width, (i+1) * heightQ);
+		doc.line(15, (i+1) * heightQ, width, (i+1) * heightQ);
 	}
+}
+function drawText(doc, textArray, qPerPage, heightQ, page){
+	doc.setTextColor(100);
+
+	for (var i = 0; i<qPerPage; i++){
+		if ((qPerPage * page + i) >= textArray.length){
+			return;
+		}
+		doc.text(20, heightQ * i + 20, (qPerPage * page + i + 1) + ")")
+		doc.text(20, heightQ * i + 40, textArray[qPerPage * page + i]);
+	}
+	
 }
 
 //Event Listener to download PDF
-function demo() {
+function drawPDF(inputArray,qPerPage) {
+	var width = 195;
+	var height = 290;
+
+	var totalQ = inputArray.length;	 
+
+	if ((totalQ < 1)){
+		throw "Must have at least one question.";
+	}
+	if (qPerPage > 10){
+		qPerPage = 10;
+	}
+
+	var numPages = Math.floor((totalQ - 1) / qPerPage) + 1;
+	var lines = qPerPage - 1;
+	var heightQ = height / qPerPage;
+
 	var doc = new jsPDF();
+	doc.setLineWidth(5);
 
 	//	draw the lines in the first page
-	drawLines(doc);
+	drawLines(doc,lines,heightQ,width);
 	//	draw the text in the first page
+	drawText(doc, inputArray, qPerPage, heightQ, 0);
 
+	// draw the lines and text on remaining pages
 	for (var i = 1; i < numPages; i++){
 		doc.addPage();
-		drawLines(doc);
+		drawLines(doc,lines,heightQ,width);
+		drawText(doc, inputArray, qPerPage, heightQ, i);
 	}
 
 	// Save the PDF
