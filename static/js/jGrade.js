69d9a097a8de499149623d9f4b99685932dec408
Xilin Liu <xal1@rice.edu>
2015-02-08 11:30:34 -0600

    asdf

--- static/js/jGrade.js
+++ static/js/jGrade.js
@@ -13,6 +13,8 @@ var keysDown = {};
 var myPos = [0,0];
 var maxIndex = 10;
 var cycleIndex = 0;
+var OFFSET_X = -c.offsetLeft + 50;
+var OFFSET_Y = -c.offsetTop;
 
 ///////////////////
 //event listeners//
@@ -20,35 +22,34 @@ var cycleIndex = 0;
 
 //Double Click Listener -- Place Text
 c.addEventListener('dblclick',function(pos){
-	myPos = pos;
-	text = "";
-	ctx.font = "30px Verdana";
-	ctx.fillStyle="#FF0000";
+    myPos = [pos[0] + OFFSET_X, pos[1] + OFFSET_Y];
+    text = "";
+    ctx.font = "30px Verdana";
+    ctx.fillStyle="#FF0000";
 });
 //Mouse Drag -- Draw
 var flag = -1;
-var lastPos = [0,0];
+var lastPos = [OFFSET_X,OFFSET_Y];
 document.addEventListener("mousedown", function(e){
     flag = 0;
 }, false);
 //1 after move
 document.addEventListener("mousemove", function(e){
-	//lastPos = xxxx.
-	if (flag == 0){
-		lastPos = [e.x,e.y];
-		flag = 1;
-	}
-	else if (flag == 1){
-	ctx.beginPath();
-	ctx.lineWidth = 10;
-	ctx.strokeStyle = '#ff0000';
-	ctx.moveTo(lastPos[0], lastPos[1]);
-	ctx.lineTo(e.x, e.y);
-	ctx.stroke();
-	lastPos = [e.x,e.y];
-	console.log(lastPos[0]);
-	flag = 1;
-	}
+    //lastPos = xxxx.
+    if (flag == 0){
+        lastPos = [e.x + OFFSET_X,e.y + OFFSET_Y];
+        flag = 1;
+    }
+    else if (flag == 1){
+        ctx.beginPath();
+        ctx.lineWidth = 10;
+        ctx.strokeStyle = '#ff0000';
+        ctx.moveTo(lastPos[0], lastPos[1]);
+        ctx.lineTo(e.x + OFFSET_X, e.y + OFFSET_Y);
+        ctx.stroke();
+        lastPos = [e.x + OFFSET_X,e.y + OFFSET_Y];
+        flag = 1;
+    }
 
 
 }, false);
@@ -66,16 +67,17 @@ document.addEventListener("mouseup", function(e){
 
 
 window.addEventListener("keydown", function(event){
-	keysDown[event.keyCode] = true;
+    keysDown[event.keyCode] = true;
 });
 
 window.addEventListener("keyup", function(event){
-	delete keysDown[event.keyCode];
+    delete keysDown[event.keyCode];
 });
 //clear button
 function clear(){
-	ctx.clearRect ( 0 , 0 , canvas.width, canvas.height );
-	getimg();
+    console.log(3)
+    ctx.clearRect ( 0 , 0 , 10000, 10000);
+    getimg();
 }
 
 ///////////
@@ -83,43 +85,43 @@ function clear(){
 ///////////
 
 window.onload = function(){
-	document.body.appendChild(canvas);
-	animate(step);
+    document.body.appendChild(canvas);
+    animate(step);
 }
 
 var step = function(){
-	// do all updates and rendering
-	update();
-	animate(step);
+    // do all updates and rendering
+    update();
+    animate(step);
 }
 
 var update = function(){
-	cycleIndex -= 1;
-	if (cycleIndex < 0){
-		cycleIndex = 0;
-	}
+    cycleIndex -= 1;
+    if (cycleIndex < 0){
+        cycleIndex = 0;
+    }
 
-	for (var key in keysDown){
+    for (var key in keysDown){
 
-		if (key == "13"){
-			// you hit enter..
-		}
+        if (key == "13"){
+            // you hit enter..
+        }
 
-		ctx.fillText(text, myPos.x, myPos.y);
-		if (cycleIndex == 0){
-			if (key == "8"){
-				// you hit backspace..
-				// text = text.substring(0, text.length - 1);
-			} 
-			else{
-				text += String.fromCharCode(key);
-				cycleIndex += maxIndex;
-			}
+        ctx.fillText(text, myPos.x, myPos.y);
+        if (cycleIndex == 0){
+            if (key == "8"){
+                // you hit backspace..
+                // text = text.substring(0, text.length - 1);
+            }
+            else{
+                text += String.fromCharCode(key);
+                cycleIndex += maxIndex;
+            }
 
-		}
-		
+        }
 
-	}
+
+    }
 };
 
 
@@ -132,17 +134,18 @@ var renderAnimate = function(imgURL, assignedQ, qPerPage) {
     // ctx.drawImage(img,0,0);
     var img = new Image();
     img.onload = function() {
-        ctx.drawImage(this, 0, (i - qPerPage * Math.floor(i / qPerPage)) * qHeight, c.width, c.height, 0, 0, c.width, c.height);
+        ctx.drawImage(this, 0, 0, 10000, 10000)
+//        ctx.drawImage(this, 0, (i - qPerPage * Math.floor(i / qPerPage)) * qHeight,
+//            c.width, c.height, OFFSET_X, OFFSET_Y, c.width, c.height);
     };
-    console.log(imgURL)
-    img.src = "/img/" + imgURL["uuid"];
-    document.getElementById("clrButton").onclick = clear;
+    img.src = "http://upload.wikimedia.org/wikipedia/commons/2/29/Large_Black_pigs.jpg"//"/img/" + imgURL["uuid"];
 
 };
+document.getElementById("clrButton").onclick = clear;
 
 ///
-var animate = window.requestAnimationFrame || 
-			window.webkitRequestAnimationFrame ||
-			window.mozRequestAnimationFrame ||
-			function(callback) { window.setTimeout(callback, 1000/60) };
+var animate = window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    function(callback) { window.setTimeout(callback, 1000/60) };
 
